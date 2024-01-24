e11486af1d4cdec7eed009632e72a11795b99455
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2024-01-24 15:36:55 +0530

    Set to the user location marker

--- components/Map/MapBoxMap.tsx
+++ components/Map/MapBoxMap.tsx
@@ -1,23 +1,36 @@
-'use client'
-import Map from 'react-map-gl';
-
+"use client";
+import { UserLocationContext } from "@/context/UserLocationContext";
+import { useContext } from "react";
+import { Map, Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapBoxMap = () => {
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
   return (
     <div className="p-3 md:p-5">
       <h2 className="text-[20px] font-semibold pb-2">Map</h2>
-     <div className=' rounded-lg overflow-hidden'> 
-     <Map
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: '100%', height: 450, borderRadius:10}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
-     </div>
+      <div className=" rounded-lg overflow-hidden">
+        {userLocation ? (
+          <Map
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+            initialViewState={{
+              longitude: userLocation?.lng,
+              latitude: userLocation?.lat,
+              zoom: 14,
+            }}
+            style={{ width: "100%", height: 450, borderRadius: 10 }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+          >
+            <Marker
+              longitude={userLocation?.lng}
+              latitude={userLocation?.lat}
+              anchor="bottom"
+            >
+              <img src="./pin.png" className="w-10 h-10"/>
+            </Marker>
+          </Map>
+        ) : null}
+      </div>
     </div>
   );
 };
